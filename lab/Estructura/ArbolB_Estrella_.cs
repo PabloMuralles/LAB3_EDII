5d92f5d76e31dda78cdd51333a72cb06dfed42a4
PabloMuralles <pablomuralles28@gmail.com>
2020-02-20 15:57:26 -0600

    cambios

--- lab/Estructura/ArbolB_Estrella_.cs
+++ lab/Estructura/ArbolB_Estrella_.cs
@@ -1,162 +1,348 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace lab3.Estructura
 {
     public class ArbolB_Estrella_
     {
-        private static ArbolB_Estrella_ _instance = null;
-        public static ArbolB_Estrella_ Instance
-        {
-            get
-            {
-                if (_instance == null) _instance = new ArbolB_Estrella_();
-                return _instance;
-            }
-        }
-        public static int grado = 5;
+       
+        int orden = 5;
+        int datosMaximos;
+        int idNodo = 2;
+        int cantidadDatos = 0;
+        bool raizLlena = false;
+        public List<Nodo> listaTempArbol = new List<Nodo>();
+
         public Nodo raiz = null;
-        public int ID { get; set; }
-        public int Proxima { get; set; }
-        static int valor_raiz = ((4 * (grado - 1)) / 3);
-
-        public void Creacion(string N, string S, int V, double P, string C_P)
+  
+        public void Insertar(Bebidas nuevo)
         {
-            var dato = new Bebidas()
-            {
-                Nombre = N,
-                Sabor = S,
-                Volumen = V,
-                Precio = P,
-                Casa_Productora = C_P
-            };
+            cantidadDatos++;
 
-            Insertar(dato);
-
-        }
-
-      
-        public void Insertar(Bebidas DatosInsertar)
-        {
-           
-            ID = 1;
             if (raiz == null)
             {
+
                 raiz = new Nodo();
-                raiz.values.Add(DatosInsertar);
+                raiz.id = 1;
+                insertar(raiz, nuevo);
+                return;
+            }
+            Nodo temporal = raiz;
+            if (cantidadDatos > orden) raizLlena = true;
+            if (raizLlena == false)
+            {
+                bool valorRepetido = false;
+              
+                if (valorRepetido == false)
+                {
+                    insertar(raiz, nuevo);
+                }
+                else { throw new ArgumentException(); }
+            }
+            if (raizLlena == true)
+            {
+                Nodo posicionLibre = new Nodo();
+                posicionLibre = buscarPosicionLibre(temporal, nuevo);
+                insertar(posicionLibre, nuevo);
+
+
+            }
+
+        }
+        public Nodo buscarPosicionLibre(Nodo nodoPosicion, Bebidas nuevo)
+        {
+            if (nodoPosicion.hijos.Count != 0)
+            {
+
+                if (nodoPosicion.valores.Count() > 1)
+                {
+                    if (nodoPosicion.valores[0].CompareTo(nuevo) == 0 || nodoPosicion.valores[1].CompareTo(nuevo) == 0 || nodoPosicion.valores[nodoPosicion.valores.Count() - 1].CompareTo(nuevo) == 0 || nodoPosicion.valores[nodoPosicion.valores.Count() - 2].CompareTo(nuevo) == 0)
+                    {
+                        return null;
+                    }
+                    if (nodoPosicion.valores[0].CompareTo(nuevo) == 1 && nodoPosicion.valores[1].CompareTo(nuevo) == 1)
+                    {
+                        return buscarPosicionLibre(nodoPosicion.hijos[0], nuevo);
+                    }
+                    if (nodoPosicion.valores[0].CompareTo(nuevo) == -1 && nodoPosicion.valores[1].CompareTo(nuevo) == 1)
+                    {
+                        return buscarPosicionLibre(nodoPosicion.hijos[1], nuevo);
+                    }
+                    if (nodoPosicion.valores[1].CompareTo(nuevo) == 1 && nodoPosicion.valores[nodoPosicion.valores.Count() - 1].CompareTo(nuevo) == -1)
+                    {
+                        return buscarPosicionLibre(nodoPosicion.hijos[2], nuevo);
+                    }
+                    if (nodoPosicion.valores[nodoPosicion.valores.Count() - 2].CompareTo(nuevo) == -1 && nodoPosicion.valores[nodoPosicion.valores.Count() - 1].CompareTo(nuevo) == -1)
+                    {
+                        return buscarPosicionLibre(nodoPosicion.hijos[nodoPosicion.valores.Count()], nuevo);
+                    }
+
+
+                }
+                else
+                {
+                    if (nodoPosicion.valores[0].CompareTo(nuevo) == +1)
+                    {
+                        return buscarPosicionLibre(nodoPosicion.hijos[0], nuevo);
+                    }
+                    if (nodoPosicion.valores[0].CompareTo(nuevo) == -1)
+                    {
+                        return buscarPosicionLibre(nodoPosicion.hijos[1], nuevo);
+                    }
+                    if (nodoPosicion.valores[0].CompareTo(nuevo) == 0 || nodoPosicion.valores[1].CompareTo(nuevo) == 0)
+                    {
+                        throw new ArgumentException();
+                    }
+
+                }
+            }
+            return nodoPosicion;
+        }
+        public void insertar(Nodo nodo, Bebidas nuevo)
+        {
+            if (nodo.hijos == null || nodo.valores.Count < datosMaximos)
+            {
+                nodo.valores.Add(nuevo);
+
+                if (nodo.id == 0)
+                {
+                    nodo.id = idNodo++;
+                }
+                ordenarValores(nodo.valores);
+                listaTempArbol.Add(nodo);
             }
             else
             {
-                if (raiz.MaximoRaiz)//full
+                nodo.valores.Add(nuevo);
+                ordenarValores(nodo.valores);
+                separarNodos(nodo);
+                listaTempArbol.Add(nodo);
+            }
+        }
+
+        void ordenarValores(List<Bebidas> aOrdenar)
+        {
+            aOrdenar.Sort();
+        }
+        void ordenarHijos(List<Nodo> hijosOrdenar)
+        {
+            Nodo aux;
+            for (int i = 0; i < hijosOrdenar.Count - 1; i++)
+            {
+                for (int j = i + 1; j < hijosOrdenar.Count; j++)
                 {
-                    // si esta lleno tira un true y se mete a este if para dividir la raiz 
-                    // si ya esta lleno dividir la raiz
-                    Primeradivision(raiz);
-                    //DividirNodo(raiz);
-                     //var NuevoElmento =Subir_Elemento(mitad, raiz.values);
+                    if (hijosOrdenar[i].valores[0].CompareTo(hijosOrdenar[j].valores[0]) == 1)
+                    {
+                        aux = hijosOrdenar[i];
+                        hijosOrdenar[i] = hijosOrdenar[j];
+                        hijosOrdenar[j] = aux;
+                    }
+                }
+            }
+
+        }
+        void ordenarLista(List<Nodo> listaOrdenar)
+        {
+            Nodo aux;
+            for (int i = 0; i < listaOrdenar.Count - 1; i++)
+            {
+                for (int j = i + 1; j < listaOrdenar.Count; j++)
+                {
+                    if (listaOrdenar[i].id.CompareTo(listaOrdenar[j].id) == 1)
+                    {
+                        aux = listaOrdenar[i];
+                        listaOrdenar[i] = listaOrdenar[j];
+                        listaOrdenar[j] = aux;
+                    }
+                }
+            }
+        }
+        void separarNodos(Nodo nodoSeparar)
+        {
+            int cantidadHijos = orden;
+            if (orden % 2 != 0)//separar a la mitad
+            {
+                Nodo temporalIzquierdo = new Nodo();
+                Nodo temporalDerecho = new Nodo();
+                Nodo temporalRaiz = new Nodo();
+                temporalDerecho.padre = nodoSeparar.padre;
+                temporalIzquierdo.padre = nodoSeparar.padre;
+                temporalRaiz.valores.Add(nodoSeparar.valores.ElementAt(datosMaximos / 2));
+                if (nodoSeparar.hijos.Count != 0)
+                {
+                    for (int i = 0; i < nodoSeparar.hijos.Count / 2; i++)
+                    {
+                        temporalIzquierdo.hijos.Add(nodoSeparar.hijos[i]);
+                    }
+                    for (int i = (nodoSeparar.hijos.Count / 2); i < nodoSeparar.hijos.Count; i++)
+                    {
+                        temporalDerecho.hijos.Add(nodoSeparar.hijos[i]);
+                    }
+                }
+                for (int i = 0; i < datosMaximos / 2; i++)
+                {
+                    temporalIzquierdo.valores.Add(nodoSeparar.valores.ElementAt(i));
+                    ordenarValores(temporalIzquierdo.valores);
+                }
+                for (int i = datosMaximos; i > (datosMaximos / 2); i--)
+                {
+                    temporalDerecho.valores.Add(nodoSeparar.valores.ElementAt(i));
+                    ordenarValores(temporalDerecho.valores);
+                }
+                if (nodoSeparar != raiz)
+                {
+                    List<Nodo> temporal = new List<Nodo>();
+                    foreach (var temp in nodoSeparar.padre.hijos)
+                    {
+                        temporal.Add(temp);
+                    }
+                    temporal.Remove(nodoSeparar);
+                    nodoSeparar.padre.hijos.Clear();
+                    nodoSeparar.padre.hijos.Add(temporalIzquierdo);
+                    nodoSeparar.padre.hijos.Add(temporalDerecho);
+                    foreach (var temp in temporal)
+                    {
+                        nodoSeparar.padre.hijos.Add(temp);
+                    }
+                    ordenarHijos(nodoSeparar.padre.hijos);
+                    nodoSeparar.padre.valores.Add(temporalRaiz.valores[0]);
+                    ordenarValores(nodoSeparar.padre.valores);
+                    ordenarHijos(nodoSeparar.padre.hijos);
+                    temporalIzquierdo.id = nodoSeparar.id;
+                    temporalDerecho.id = idNodo++;
+                    if (nodoSeparar.padre.valores.Count > datosMaximos)
+                    {
+                        separarNodos(nodoSeparar.padre);
+                    }
+                }
+                if (nodoSeparar == raiz)
+                {
+                    raiz.valores.Clear();
+                    temporalRaiz.id = nodoSeparar.id;
+                    raiz = temporalRaiz;
+                    temporalDerecho.padre = raiz;
+                    temporalIzquierdo.padre = raiz;
+                    temporalIzquierdo.id = idNodo++;
+                    temporalDerecho.id = idNodo++;
+                    if (temporalIzquierdo.hijos.Count != 0)
+                    {
+                        foreach (var hijos in temporalIzquierdo.hijos)
+                        {
+                            hijos.padre = temporalRaiz;
+                        }
+                    }
+                    if (temporalDerecho.hijos.Count != 0)
+                    {
+                        foreach (var hijos in temporalDerecho.hijos)
+                        {
+                            hijos.padre = temporalRaiz;
+                        }
+                    }
+                    raiz.hijos.Add(temporalIzquierdo);
+                    raiz.hijos.Add(temporalDerecho);
+                }
+
+            }
+
+
+        }
+       
+      
+      
+        public void buscar(Bebidas datoBuscado)
+        {
+            Nodo nodoBuscado = buscarValor(datoBuscado, raiz);
+            int indiceValor = 0;
+            foreach (var datos in nodoBuscado.valores)
+            {
+                if (datoBuscado.CompareTo(datos) == 0)
+                {
+                    return;
+                }
+                else { indiceValor++; }
+            }
+
+        }
+        
+        public void listaTemp()
+        {
+            if (listaTempArbol.Count == 0)
+            {
+                listaTempArbol.Add(raiz);
+            }
+            foreach (var nodo in raiz.hijos)
+            {
+                listaTempArbol.Add(nodo);
+                //while (nodo.hijos.Count != 0)
+                {
+                    for (int i = 0; i < nodo.hijos.Count; i++)
+                    {
+                        if (listaTempArbol.Contains(nodo.hijos[i]) == false)
+                        {
+                            listaTempArbol.Add(nodo.hijos[i]);
+                        }
+
+                    }
+                }
+            }
+        }
+        public void recorrido(Nodo nodito)
+        {
+            if (!ArbolVacio(nodito))
+            {
+                if (nodito.hijos.Count != 0)
+
+                {
+                    recorrido(nodito.hijos[0]);
+                    for (int i = 0; i < nodito.hijos.Count; i++)
+                    {
+                        listaTempArbol.Add(nodito.hijos[i]);
+                        recorrido(nodito.hijos[i]);
+                    }
+
                 }
                 else
-                { 
-                    // sino esta al maximo tira un false que se mete al else para nada mas insertarlo
-                    raiz.values.Add(DatosInsertar);
-                    raiz.values = Ordenar(raiz.values);
+                {
+                    if (listaTempArbol.Contains(nodito) == false)
+                    {
+                        listaTempArbol.Add(nodito);
+                    }
                 }
             }
         }
-        public void Primeradivision(Nodo PrimerNodo)
+        public static bool ArbolVacio(Nodo nodito) => nodito == null;
+        public void archivoTexto(string nombreArchivo)
         {
-            int mitad = PrimerNodo.values.Count / 2;
-            for (int i = 0; i < grado + 1; i++)
+            using (StreamWriter writer = new StreamWriter("C:/microSQL/" + nombreArchivo + "B.txt"))
             {
-              
-                for (int j = 0; j < mitad; j++)
+                writer.WriteLine("Orden " + orden);
+                writer.WriteLine("Raiz " + raiz.id);
+                writer.WriteLine("Posicion " + idNodo);
+                listaTemp();
+                ordenarLista(listaTempArbol);
+                foreach (var nodo in listaTempArbol)
                 {
-                     PrimerNodo.hijos[i].values[i] = new Bebidas();
-                    PrimerNodo.hijos[i].values.Add(PrimerNodo.values[j]);
+                    if (nodo.padre == null)
+                    {
+                        writer.Write(nodo.id + "|0|");
+                    }
+                    else
+                    {
+                        writer.Write(nodo.id + "|" + nodo.padre.id + "|");
+                    }
+                    foreach (var nodosHijos in nodo.hijos)
+                    {
+                        writer.Write(nodosHijos.id + "|");
+                    }
+                    foreach (var valores in nodo.valores)
+                    {
+                        writer.Write(valores + "|");
+                    }
                 }
-                for (int k = PrimerNodo.hijos[i].values.Count -1 ; i < mitad ; k--)
-                {
-                    PrimerNodo.hijos[i + 1].values.Add(PrimerNodo.values[k]);
-                }               
-            }            
-        }
-        public void DividirNodo(Nodo NodoSeparar)
-        {
-            int mitad = NodoSeparar.values.Count / 2;
-            int DatosMaximos = grado - 1;
-            int CantidadHijos = grado;
 
-            Nodo TempIzquierdo = new Nodo();
-            Nodo TempDerecho = new Nodo();
-            Nodo TempPadre = new Nodo();
-
-            TempDerecho.padre = NodoSeparar.padre;
-            TempIzquierdo.padre = NodoSeparar.padre;
-            TempPadre.values.Add(Subir_Elemento(mitad,NodoSeparar.values));
-
-            if (NodoSeparar.hijos.Count!=0)
-            {
-                for (int i = 0; i < NodoSeparar.hijos.Count/2; i++)
-                {
-                    TempIzquierdo.hijos.Add(NodoSeparar.hijos[i]);
-
-                }
-                for (int i = NodoSeparar.hijos.Count / 2; i < NodoSeparar.hijos.Count; i++)
-                {
-                    TempDerecho.hijos.Add(NodoSeparar.hijos[i]);
-
-                }
-            }
-            for (int i = 0; i < mitad; i++)
-            {
-                TempIzquierdo.values.Add(NodoSeparar.values[i]);
-                TempIzquierdo.values = Ordenar(TempIzquierdo.values);
-            }
-            for (int i = NodoSeparar.values.Count-1; i < mitad; i--)
-            {
-                TempDerecho.values.Add(NodoSeparar.values[i]);
-                TempDerecho.values = Ordenar(TempDerecho.values);
-            }
-
-
-
-        }
-        public List<Bebidas> Ordenar(List<Bebidas> NodoOrdenar)
-        {
-            var ListaOrdenada=NodoOrdenar.OrderBy(x => x.Nombre).ToList();
-            return ListaOrdenada;
-        }
-        public Bebidas Subir_Elemento(int mitad, List<Bebidas> nodo)
-        {
-
-            int num = 0;
-            var nuevo_elemento = new Bebidas();
-            foreach (var item in nodo)
-            {
-                if (num == mitad)
-                {
-                    nuevo_elemento = item;
-
-                    break;
-                }
-                num++;
-            }
-            return nuevo_elemento;
-        }
-
-        // metodo para poder limpiar la raiz y asignarle el de en medio
-        public void Limpiar()
-        {
-            raiz = new Nodo();
-        }
-
-        public void InsertarElemento (Nodo NodoActual)
-        {
-            if (NodoActual.EsHoja)
-            {
-              
             }
         }
 
--- lab/Nodo.cs
+++ lab/Nodo.cs
@@ -7,43 +7,11 @@ namespace lab3
 {
     public class Nodo
     {
-        
-        public int id { get; set; }
-        public Nodo padre { get; set; }
-        public List<Nodo> hijos { get; set; }
-        public List<Bebidas> values { get; set; }
-        public static int grado = 5;
-        public Nodo()
-        {
-            hijos = new List<Nodo>();
-            values = new List<Bebidas>();
-           
-            
-             
-        }
 
-       
-        public int MAX()
-        {
-            int max = (4*(grado-1)/3);
-            if (max%2!=0)
-            {
-                max = max + 1;
-            }
-            return max;
-        }
-
-        public bool EsHoja => hijos.Count == 0;
-
-        public bool Minimo => values.Count == ((2 * grado) - 1) / 3;
-
-        public bool Maximo => values.Count == grado - 1;
-
-       
-        public bool MaximoRaiz => values.Count == MAX();
-
-
-
+        public Nodo padre;
+        public int id;
+        public List<Bebidas> valores = new List<Bebidas>();
+        public List<Nodo> hijos = new List<Nodo>();
 
 
     }
